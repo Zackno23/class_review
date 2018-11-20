d7e54cb548e2d697033c6258eb31c64f84808efe
chikara <chikara.f.yoshida@gmail.com>
2018-11-20 21:57:42 +0900

    TypeError:'int'object is not callable 関数名と変数名を一緒にしてはいけないと、ぐぐると書いてある。 どういうこと?

--- b_1.py
+++ b_1.py
@@ -1,13 +1,23 @@
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.toshi = age
 
     def full_name(self):
         print(self.first_name + self.family_name)
 
+    def age(self):
+        toshi = self.toshi
+        print(toshi)
 
-ken = Customer('Ken', 'Tanaka')
+
+ken = Customer('Ken', 'Tanaka', 15)
 ken.full_name()
-tom = Customer('Tom', 'Ford')
+ken.age()
+tom = Customer('Tom', 'Ford', 57)
 tom.full_name()
+tom.age()
+ieyasu = Customer('Ieyasu', 'Tokugawa', 73)
+tom.full_name()
+ieyasu.age()
